c426744bd01a185f8578138c7f197f788966e38a
Pavan2005-v <vpavan1242005@gmail.com>
2025-12-09 19:02:04 +0530

    Time: 0 ms (100%), Space: 35.3 MB (28.99%) - LeetHub

--- 3131-find-the-integer-added-to-array-i/3131-find-the-integer-added-to-array-i.cpp
+++ 3131-find-the-integer-added-to-array-i/3131-find-the-integer-added-to-array-i.cpp
@@ -3,6 +3,6 @@ public:
     int addedInteger(vector<int>& nums1, vector<int>& nums2) {
         sort(nums1.begin(),nums1.end());
         sort(nums2.begin(),nums2.end());
-        return nums2[0]-nums1[0];
+        return nums2[0]-nums1[0];               
     }
 };
