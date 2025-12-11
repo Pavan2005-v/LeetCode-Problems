224c28215173666b38ffe7c91ec13ad35f1c217e
Pavan2005-v <vpavan1242005@gmail.com>
2025-12-11 23:38:24 +0530

    Time: 3 ms (42.28%), Space: 27.7 MB (73.75%) - LeetHub

---
+++ 2932-maximum-strong-pair-xor-i/2932-maximum-strong-pair-xor-i.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maximumStrongPairXor(vector<int>& nums) {
+        int maxi=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            for(int j=0;j<nums.size();j++)
+            {
+                if(abs(nums[i]-nums[j])<=min(nums[i],nums[j]))
+                {
+                    if((nums[i]^nums[j])>maxi) maxi=nums[i]^nums[j];
+                }
+            }
+        }
+        return maxi;       
+    }
+};
