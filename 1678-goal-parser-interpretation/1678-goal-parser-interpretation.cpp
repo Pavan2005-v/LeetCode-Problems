895979aea4aaf2ec951389eafd5ecf2b9766e671
Pavan2005-v <vpavan1242005@gmail.com>
2025-12-14 10:36:17 +0530

    Time: 3 ms (42.01%), Space: 8.3 MB (15.48%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string interpret(string command) {
+        string ans="";
+        for(int i=0;i<command.size();i++)
+        {
+            if(command[i]=='G') ans+='G';
+            else if(command[i]=='('&&command[i+1]==')') ans+='o';
+            else if(command[i]=='('&&command[i+3]==')') ans+="al";
+        } 
+        return ans;       
+    }
+};
