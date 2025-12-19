daca7493c501f872a7b2c1c0cf2c9ce79f1e9688
Pavan2005-v <vpavan1242005@gmail.com>
2025-12-19 09:47:16 +0530

    Time: 5 ms (57.04%), Space: 14.4 MB (27.7%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        stack<char>st;
+        for(int i=0;i<s.length();i++)
+        {
+            if(st.size()>=0)
+            {
+                if(st.size()==0) st.push(s[i]);
+                else if(st.top()!=s[i]) st.push(s[i]);
+                else if(st.top()==s[i]) st.pop();
+            }
+        }
+        string ans="";
+        while(st.size()!=0)
+        {
+            ans+=(st.top());
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
