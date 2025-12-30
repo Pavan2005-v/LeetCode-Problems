30222a4075a6afffc526421d812b177408542f99
Pavan2005-v <vpavan1242005@gmail.com>
2025-12-30 20:38:50 +0530

    Time: 0 ms (100%), Space: 8.2 MB (43.78%) - LeetHub

---
+++ 2651-calculate-delayed-arrival-time/2651-calculate-delayed-arrival-time.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
+        int sum=arrivalTime+delayedTime;
+        if(sum<24) return sum;
+        else if(sum==24) return 0;
+        else
+        {
+            return sum-24;
+        }
+    }
+};
