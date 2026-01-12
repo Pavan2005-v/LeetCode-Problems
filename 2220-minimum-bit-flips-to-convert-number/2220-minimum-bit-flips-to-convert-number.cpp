0f1a1831617092c51b7b78e0371a7668914e555a
Pavan2005-v <vpavan1242005@gmail.com>
2026-01-12 22:21:50 +0530

    Time: 0 ms (100%), Space: 7.9 MB (59.02%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -1,31 +1,19 @@
 class Solution {
 public:
     int minBitFlips(int start, int end) {
-        string temp1="";
-        string temp2="";
-        while(start>0)
+        int count=0;
+        while(start>0&&end>0)
         {
-            temp1+=((start%2)+'0');
+            if((start%2)!=(end%2)) count++;
             start/=2;
-        }
-        while(end>0)
-        {
-            temp2+=((end%2)+'0');
             end/=2;
         }
-        if(temp1.size()>temp2.size())
+        while(start>0||end>0)
         {
-            while(temp1.size()!=temp2.size()) temp2+='0';
+            if(start%2!=end%2) count++;
+            start/=2;
+            end/=2;
         }
-        else if(temp1.size()<temp2.size())
-        {
-            while(temp1.size()!=temp2.size()) temp1+='0';
-        }
-        int ans=0;
-        for(int i=0;i<temp1.size();i++)
-        {
-            if(temp1[i]!=temp2[i]) ans++;
-        }
-        return ans;
+        return count;
     }
 };
