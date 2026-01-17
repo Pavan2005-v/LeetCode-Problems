3c0971ea32e632c2d2ce2170f23fab67a8b94c7d
Pavan2005-v <vpavan1242005@gmail.com>
2026-01-18 00:18:38 +0530

    Time: 90 ms (75.69%), Space: 257.5 MB (58.14%) - LeetHub

--- 3810-minimum-operations-to-reach-target-array/3810-minimum-operations-to-reach-target-array.cpp
+++ 3810-minimum-operations-to-reach-target-array/3810-minimum-operations-to-reach-target-array.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, vector<int>& target) {
-        int count=0;
         unordered_map<int,int>mpp;
         for(int i=0;i<nums.size();i++)
         {
