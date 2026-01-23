19ca13a6898965ed1d4a4c5df45eca724279d293
Pavan2005-v <vpavan1242005@gmail.com>
2026-01-23 11:01:10 +0530

    Time: 71 ms (5.13%), Space: 109.9 MB (5.04%) - LeetHub

--- 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.cpp
+++ 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    bool isValid(vector<int>nums,int mid,int balls)
+    bool isValid(vector<int>nums,int allowed,int k)
     {
         int pos=nums[0];
         int c=1;
         for(int i=1;i<nums.size();i++)
         {
-            if((nums[i]-pos)>=mid)
+            if(nums[i]-pos>=allowed)
             {
                 c++;
                 pos=nums[i];
             }
         }
-        cout<<mid<<" "<<c<<endl;
-        return c>=balls;
+        return c>=k;
     }
     int maxDistance(vector<int>& nums, int m) {
         sort(nums.begin(),nums.end());
         int i=1;
         int j=nums[nums.size()-1]-nums[0];
-        int ans=-1;
+        int ans;
         while(i<=j)
         {
             int mid=i+(j-i)/2;
@@ -31,5 +30,6 @@ public:
             else j=mid-1;
         }
         return ans;
+
     }
 };
