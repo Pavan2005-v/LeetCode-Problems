94158320b7d4fcb9b0f2cf652dfd22861c7ab259
Pavan2005-v <vpavan1242005@gmail.com>
2026-01-24 22:25:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.cpp
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.cpp
@@ -2,15 +2,26 @@ class Solution {
 public:
     int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
         int sum=0;
-        for(int i=0;i<worker.size();i++)
+        vector<pair<int,int>>nums;
+        for(int i=0;i<profit.size();i++)
         {
+            nums.push_back({profit[i],difficulty[i]});
+        }
+        sort(nums.begin(),nums.end());
+        for(int k=0;k<worker.size();k++)
+        {
+            int i=0;
+            int j=nums.size()-1;
             int ele=0;
-            for(int j=0;j<difficulty.size();j++)
+            while(i<=j)
             {
-                if(worker[i]>=difficulty[j])
+                int mid=i+(j-i)/2;
+                if(worker[k]>=nums[mid].second)
                 {
-                    ele=max(ele,profit[j]);
+                    ele=nums[mid].first;
+                    i=mid+1;
                 }
+                else j=mid-1;
             }
             sum+=ele;
         }
