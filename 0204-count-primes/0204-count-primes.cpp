4123de366f041c3f78e82df202616ddae8396096
Pavan2005-v <vpavan1242005@gmail.com>
2026-01-26 16:13:56 +0530

    Time: 453 ms (6.98%), Space: 132.7 MB (9.01%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        vector<int>temp(n,1);
+        for(int i=2;i*i<=n;i++)
+        {
+            int j=2;
+            while((j*i)<n)
+            {
+                temp[(j*i)]=0;
+                j++;
+            }
+        }
+        // for(int i:temp) cout<<i<<" ";
+        int ans=0;
+        for(int i=2;i<temp.size();i++) if(temp[i]==1) ans++;
+        return ans;
+    }
+};
