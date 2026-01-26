9b0599ecd275deafe0c62745ae8fc997e1312b38
Pavan2005-v <vpavan1242005@gmail.com>
2026-01-26 16:52:51 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     int countPrimes(int n) {
-        vector<int>temp(n,1);
-        for(int i=2;i*i<=n;i++)
-        {
-            if(temp[i]==0) continue;
-            int j=2;
-            while((j*i)<n)
-            {
-                temp[(j*i)]=0;
-                j++;
-            }
-        }
+        vector<bool>temp(n,1);
         int ans=0;
-        for(int i=2;i<temp.size();i++) if(temp[i]==1) ans++;
-        return ans;
+        for(int i=2;i<n;i++)
+        {
+            if(temp[i])
+            {
+                ans++;
+                for(int j=i*2;j<n+1;j+=i) temp[j]=0;
+            }
+        }   
+       return ans;
     }
 };
