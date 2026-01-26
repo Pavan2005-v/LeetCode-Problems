42f2f94f8c3d53bed58ef13a5bc77e9d64c3481b
Pavan2005-v <vpavan1242005@gmail.com>
2026-01-26 14:12:38 +0530

    Time: 21 ms (41.9%), Space: 93.4 MB (31.31%) - LeetHub

--- 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.cpp
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.cpp
@@ -1,28 +1,48 @@
 class Solution {
 public:
     int maximumBags(vector<int>& capacity, vector<int>& rocks, int Rocks) {
-        vector<pair<int,vector<int>>> nums;
+        // vector<pair<int,vector<int>>> nums;
+        // for(int i=0;i<capacity.size();i++)
+        // {
+        //     nums.push_back({capacity[i]-rocks[i],{capacity[i],rocks[i]}});
+        // }
+        // sort(nums.begin(),nums.end());
+        // for(auto &it:nums)
+        // {
+        //     if(it.first!=0&&Rocks>0)
+        //     {
+        //         if(Rocks-it.first>=0)
+        //         {
+        //             it.second[1]+=it.first;
+        //             Rocks-=it.first;
+        //             it.first=0;
+        //         }
+        //     }
+        // }
+        // int ans=0;
+        // for(auto it:nums)
+        // {
+        //     if(it.first==0) ans++;
+        // }
+        // return ans;
+        vector<int>nums;
         for(int i=0;i<capacity.size();i++)
         {
-            nums.push_back({capacity[i]-rocks[i],{capacity[i],rocks[i]}});
-        }
-        sort(nums.begin(),nums.end());
-        for(auto &it:nums)
-        {
-            if(it.first!=0&&Rocks>0)
-            {
-                if(Rocks-it.first>=0)
-                {
-                    it.second[1]+=it.first;
-                    Rocks-=it.first;
-                    it.first=0;
-                }
-            }
+            nums.push_back(capacity[i]-rocks[i]);
         }
         int ans=0;
-        for(auto it:nums)
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++)
         {
-            if(it.first==0) ans++;
+            if(nums[i]==0) ans++;
+            else
+            {
+                if(Rocks-nums[i]>=0) 
+                {
+                    Rocks-=nums[i];
+                    ans++;
+                }
+            }
         }
         return ans;
     }
