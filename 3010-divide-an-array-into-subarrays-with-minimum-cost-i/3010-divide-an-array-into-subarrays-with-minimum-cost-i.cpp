89690ff7b749123c485099afe4404a3b7fdc9992
Pavan2005-v <vpavan1242005@gmail.com>
2026-02-01 09:06:23 +0530

    Time: 0 ms (100%), Space: 30.4 MB (74.87%) - LeetHub

--- 3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.cpp
+++ 3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.cpp
@@ -1,26 +1,9 @@
 class Solution {
 public:
     int minimumCost(vector<int>& nums) {
-        int ans=nums[0];
-        int ind;
-        int ele=100;
-        for(int i=1;i<nums.size();i++)
-        {
-            if(ele>nums[i])
-            {
-                ele=nums[i];
-                ind=i;
-            }
-        }
-        int ele1=100;
-        for(int i=1;i<nums.size();i++)
-        {
-            if(i!=ind)
-            {
-                ele1=min(ele1,nums[i]);
-            }
-        }
-        return ans+ele+ele1;
+        int sum=nums[0];
+        sort(nums.begin()+1,nums.end());
+        return sum+nums[1]+nums[2];
         
     }
 };
