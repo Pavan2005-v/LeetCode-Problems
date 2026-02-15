b9e4492d0021fe126506afd5218ff136902a8df7
V PAVAN SATYA PRASAD <vpavan1242005@gmail.com>
2026-02-16 00:07:36 +0530

    Time: 35 ms (50.72%), Space: 32.6 MB (36.84%) - LeetHub

--- 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.cpp
+++ 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.cpp
@@ -1,40 +1,19 @@
 class Solution {
 public:
     int minimumLength(string s) {
-        // string ex="tempo";
-        // ex.erase(2,1);
-        // cout<<ex;
-        // return 1;
+        
         unordered_map<char,int>mpp;
         for(char c:s) mpp[c]++;
-        int count=s.length();
-        int i=0;
-        int j=1;
-        if(s.length()<=2) return s.length();
-        while(j<s.length())
+        int ans=0;
+        for(auto it:mpp)
         {
-            if(s[i]==s[j])
+            if(it.second>=3)
             {
-                int k=j;
-                while(k<s.length())
-                {
-                    k++;
-                    if(s[k]==s[j])
-                    {
-                        s.erase(k,1);
-                        s.erase(i,1);
-                        i=0;
-                        j=1;
-                        cout<<s<<endl;
-                    }
-
-                }
+                if(it.second%2) ans++;
+                else ans+=2;
             }
-            else 
-            {
-                j++;
-            }
-        }
-        return s.length();
+            else ans+=it.second;
+        } 
+        return ans;
     }
 };
