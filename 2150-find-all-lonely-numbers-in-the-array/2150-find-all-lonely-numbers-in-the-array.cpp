ffa213ad56410685097c96a1284c5f168b82462c
V PAVAN SATYA PRASAD <vpavan1242005@gmail.com>
2026-02-19 22:19:37 +0530

    Time: 159 ms (66.43%), Space: 171.4 MB (80.84%) - LeetHub

--- 2150-find-all-lonely-numbers-in-the-array/2150-find-all-lonely-numbers-in-the-array.cpp
+++ 2150-find-all-lonely-numbers-in-the-array/2150-find-all-lonely-numbers-in-the-array.cpp
@@ -2,21 +2,16 @@ class Solution {
 public:
     vector<int> findLonely(vector<int>& nums) {
         unordered_map<int,int>mpp;
-        unordered_set<int>st;
         vector<int>ans;
         for(int i:nums)
         {
             mpp[i]++;
-            st.insert(i);
         }
         for(auto it:mpp)
         {
-            if(it.second==1)
+            if(it.second==1&&!mpp.contains(it.first+1)&&!mpp.contains(it.first-1))
             {
-                if(!st.contains(it.first+1)&&!st.contains(it.first-1))
-                {
-                    ans.push_back(it.first);
-                }
+                ans.push_back(it.first);
             }
         }
         return ans;
