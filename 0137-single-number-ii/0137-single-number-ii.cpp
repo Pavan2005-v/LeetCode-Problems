849f5c5e1f5922da216c2092750955803b784512
V PAVAN SATYA PRASAD <vpavan1242005@gmail.com>
2026-02-26 21:58:51 +0530

    Time: 0 ms (100%), Space: 13.3 MB (86.19%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -3,7 +3,7 @@ public:
     int singleNumber(vector<int>& nums) {
         int ans=0;
         int n=nums.size();
-        for(int i=0;i<31;i++)
+        for(int i=0;i<=31;i++)
         {
             int count=0;
             for(int j=0;j<n;j++)
