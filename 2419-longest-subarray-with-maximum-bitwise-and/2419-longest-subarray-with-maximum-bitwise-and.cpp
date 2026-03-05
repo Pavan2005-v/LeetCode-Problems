d0c3b379d90e18e3f758c68e344e2eb67ad98429
V PAVAN SATYA PRASAD <vpavan1242005@gmail.com>
2026-03-05 23:07:45 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2419-longest-subarray-with-maximum-bitwise-and/2419-longest-subarray-with-maximum-bitwise-and.cpp
+++ 2419-longest-subarray-with-maximum-bitwise-and/2419-longest-subarray-with-maximum-bitwise-and.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
-        int maxi=*max_element(nums.begin(),nums.end());
+        int maxi=INT_MIN;
+        for(int i:nums) maxi=max(maxi,i);
         int c=1;
         int ans=0;
         for(int i=0;i<nums.size()-1;i++)
