885d15ebffa9c091ec1a2aacc90ce94a8fd785a2
V PAVAN SATYA PRASAD <vpavan1242005@gmail.com>
2026-03-09 23:33:19 +0530

    Time: 0 ms (100%), Space: 7.8 MB (81.01%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,13 +1,7 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n<0) return false;
-        while(n>1)
-        {
-            if(n%2!=0) return false;
-            n/=2;
-        }
-        if(n) return true;
-        else return false;
+        if(n<=0) return false;
+        return (n>0&&(n&(n-1)))==0;
     }
 };
