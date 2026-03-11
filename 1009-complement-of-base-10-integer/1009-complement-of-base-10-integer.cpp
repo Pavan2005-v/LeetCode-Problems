38eeb312cffd0ae37f31776bb3fe0a43684a3698
V PAVAN SATYA PRASAD <vpavan1242005@gmail.com>
2026-03-11 11:31:29 +0530

    Time: 0 ms (100%), Space: 7.9 MB (59.24%) - LeetHub

---
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        int ans=0;
+        int ind=0;
+        if(n==0) return 1;
+        while(n>0)
+        {
+            if(!(n&1)) 
+            {
+                ans|=(1<<ind);
+            }
+            ind++;
+            n>>=1;
+        }
+        return ans;
+    }
+};
