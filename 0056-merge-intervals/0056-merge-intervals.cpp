bc075f21ffeb5f283867e4c293f02ca803da894a
V PAVAN SATYA PRASAD <vpavan1242005@gmail.com>
2026-03-17 22:26:21 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -5,31 +5,24 @@ public:
         vector<vector<int>>ans;
         int temp=intervals[0][1];
         int ind=0;
-        int num=intervals[0][1];
-        for(auto it:intervals)
-        {
-            for(int i:it) cout<<i<<" ";
-            cout<<endl;
-        }
-        for(int i=0;i<intervals.size();i++)
+        for(int i=1;i<intervals.size();i++)
         {
             if(intervals[i][0]>temp)
             {
-                ans.push_back({intervals[ind][0],num});
+                ans.push_back({intervals[ind][0],temp});
                 ind=i;
-                num=0;
+                temp=intervals[i][1];
             }
             temp=max(temp,intervals[i][1]);
-            num=max(num,intervals[i][1]);
         }
         int n=intervals.size()-1;
         if(ind!=n)
         {
-            ans.push_back({intervals[ind][0],num});
+            ans.push_back({intervals[ind][0],temp});
         }
         else
         {
-            ans.push_back({intervals[n][0],num});
+            ans.push_back({intervals[n][0],temp});
         }
         return ans;
     }
