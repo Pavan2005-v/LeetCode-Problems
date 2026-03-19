a4cc25b5f97f5cdee784f2f67823c252b06fce10
V PAVAN SATYA PRASAD <vpavan1242005@gmail.com>
2026-03-19 15:29:18 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,25 +2,32 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if(s.length()!=t.length()) return false;
-        unordered_map<char,int>mpp1;
-        unordered_map<char,int>mpp2;
-        for(char c:s) mpp1[c]++;
-        for(char c:t) mpp2[c]++;
-        for(auto it:mpp1)
+        // unordered_map<char,int>mpp1;
+        // unordered_map<char,int>mpp2;
+        // for(char c:s) mpp1[c]++;
+        // for(char c:t) mpp2[c]++;
+        // for(auto it:mpp1)
+        // {
+        //     if(mpp2.contains(it.first))
+        //     {
+        //         if(mpp1[it.first]!=mpp2[it.first]) return false;
+        //     }
+        //     else return false;
+        // }
+        // for(auto it:mpp2)
+        // {
+        //     if(mpp1.contains(it.first))
+        //     {
+        //         if(mpp1[it.first]!=mpp2[it.first]) return false;
+        //     }
+        //     else return false;
+        // }
+        // return true;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        for(int i=0;i<s.length();i++)
         {
-            if(mpp2.contains(it.first))
-            {
-                if(mpp1[it.first]!=mpp2[it.first]) return false;
-            }
-            else return false;
-        }
-        for(auto it:mpp2)
-        {
-            if(mpp1.contains(it.first))
-            {
-                if(mpp1[it.first]!=mpp2[it.first]) return false;
-            }
-            else return false;
+            if(s[i]!=t[i]) return false;
         }
         return true;
     }
