cbcdd1fa93a0037a07d7dd89266d65a33a2d06e3
V PAVAN SATYA PRASAD <vpavan1242005@gmail.com>
2026-03-24 15:22:05 +0530

    Time: 0 ms (100%), Space: 42.2 MB (25.16%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int>ans(nums.size());
-        vector<int>pp(nums.size());
-        vector<int>sp(nums.size());
-        pp[0]=nums[0];
-        for(int i=1;i<nums.size();i++) pp[i]=pp[i-1]*nums[i];
-        sp[sp.size()-1]=nums[sp.size()-1];
-        for(int i=nums.size()-2;i>=0;i--)
+        vector<int>pref(nums.size());
+        vector<int>suf(nums.size());
+        pref[0]=nums[0];
+        pref[1]=nums[0];
+        int n=nums.size();
+        for(int i=2;i<nums.size();i++)
         {
-            sp[i]=sp[i+1]*nums[i];
+            pref[i]=pref[i-1]*nums[i-1];
         }
-        ans[0]=sp[1];
-        ans[nums.size()-1]=pp[nums.size()-2];
+        suf[n-1]=nums[n-1];
+        suf[n-2]=nums[n-1];
+        for(int i=n-3;i>=0;i--)
+        {
+            suf[i]=suf[i+1]*nums[i+1];
+        }
+        vector<int>ans(nums.size());
+        ans[0]=suf[0];
+        ans[n-1]=pref[n-1];
         for(int i=1;i<nums.size()-1;i++)
         {
-            ans[i]=pp[i-1]*sp[i+1];
+            ans[i]=pref[i]*suf[i];
         }
         return ans;
     }
