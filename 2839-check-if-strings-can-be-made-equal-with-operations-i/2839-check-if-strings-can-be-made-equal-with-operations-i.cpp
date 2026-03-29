288a9f6fdd945bcce54fcc5ced885788757809a5
V PAVAN SATYA PRASAD <vpavan1242005@gmail.com>
2026-03-29 11:56:11 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.cpp
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     bool canBeEqual(string s1, string s2) {
-        for(int i=0;i<4;i++)
+        for(int i=0;i<2;i++)
         {
-            if(s1[i]!=s2[i]&&i+2<4&&s1[i+2]==s2[i])
+            if(s1[i]!=s2[i])
             {
                 swap(s1[i],s1[i+2]);  
             }
