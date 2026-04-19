aff0079a1e9a867fc14dec85c549251bcc482246
V PAVAN SATYA PRASAD <vpavan1242005@gmail.com>
2026-04-19 10:42:29 +0530

    Time: 56 ms (5.26%), Space: 31.3 MB (52.3%) - LeetHub

---
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n=nums.size()-1;
+        vector<int>temp(n+1);
+        temp[n]=nums[n];
+        for(int i=n-1;i>=0;i--)
+        {
+            temp[i]=min(temp[i+1],nums[i]);
+        }
+        for(int i:temp) cout<<i<<" ";
+        int num=nums[0];
+        for(int i=0;i<=n;i++)
+        {
+            if(num-temp[i]<=k) return i;
+            num=max(nums[i],num);
+        }
+        return -1;
+    }
+};
