678d10fdb381c48d506bcdd618751a5f52e6ed3c
V PAVAN SATYA PRASAD <vpavan1242005@gmail.com>
2026-05-21 15:15:18 +0530

    Time: 0 ms (100%), Space: 8.3 MB (35.08%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -2,14 +2,8 @@ class Solution {
 public:
     bool rotateString(string s, string goal) {
         if(s.length()!=goal.length()) return false;
-        int it=0;
-        while(it<s.length())
-        {
-            s+=s[0];
-            s.erase(0,1);
-            if(s==goal) return true;
-            it++;
-        }
-        return false;
+        s+=s;
+        if(s.contains(goal)) return true;
+        else return false;
     }
 };
