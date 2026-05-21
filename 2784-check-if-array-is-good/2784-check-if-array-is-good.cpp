0851dde5ef1b66c774cb299175cbebaf84caa94f
V PAVAN SATYA PRASAD <vpavan1242005@gmail.com>
2026-05-21 18:32:55 +0530

    Time: 3 ms (17.28%), Space: 26.5 MB (67.56%) - LeetHub

--- 2784-check-if-array-is-good/2784-check-if-array-is-good.cpp
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.cpp
@@ -2,15 +2,12 @@ class Solution {
 public:
     bool isGood(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        if(nums.size()<=1) return false;
-        if(nums[0]!=1) return false;
-        for(int i=0;i<nums.size()-2;i++)
+        int num=0,n=nums.size()-1;
+        for(int i=0;i<n;i++)
         {
-            if(nums[i]!=nums[i+1]-1)
-            {
-                return false;
-            }
+            num++;
+            if(nums[i]!=num) return false;
         }
-        return nums[nums.size()-1]==nums[nums.size()-2];
+        return nums[n]==num;
     }
 };
