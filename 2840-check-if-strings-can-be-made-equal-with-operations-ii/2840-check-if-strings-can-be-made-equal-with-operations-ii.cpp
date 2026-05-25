1b95f24102b7f8218c0345a31c2a69a5baa6864d
V PAVAN SATYA PRASAD <vpavan1242005@gmail.com>
2026-05-25 15:30:28 +0530

    Time: 44 ms (27.58%), Space: 19.6 MB (75.51%) - LeetHub

--- 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
     bool checkStrings(string s1, string s2) {
-        string st1="";
-        string st2="";
-        string stt1="";
-        string stt2="";
+        vector<int>even(26);
+        vector<int>odd(26);
         for(int i=0;i<s1.length();i++)
         {
             if(i&1)
             {
-                st1+=s1[i];
+                even[s1[i]-'a']++;
             }
-            else st2+=s1[i];
+            else odd[s1[i]-'a']++;
         }
         for(int i=0;i<s2.length();i++)
         {
             if(i&1)
             {
-                stt1+=s2[i];
+                even[s2[i]-'a']--;
             }
-            else stt2+=s2[i];
+            else odd[s2[i]-'a']--;
         }
-        sort(st1.begin(),st1.end());
-        sort(stt1.begin(),stt1.end());
-        sort(stt2.begin(),stt2.end());
-        sort(st2.begin(),st2.end());
-        return st1==stt1&&st2==stt2;
+        for(int i:even)
+        {
+            if(i!=0) return false;
+        }
+        for(int i:odd) 
+        {
+            if(i!=0) return false;
+        }
+        return true;
     }
 };
