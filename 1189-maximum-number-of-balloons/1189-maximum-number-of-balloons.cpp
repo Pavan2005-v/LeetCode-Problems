1e206d0beeb92311955ec3d779cd63b5748a547d
V PAVAN SATYA PRASAD <vpavan1242005@gmail.com>
2026-05-31 21:17:42 +0530

    Time: 0 ms (100%), Space: 9 MB (60.55%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        unordered_map<char,int>mpp;
+        for(char c:text)
+        {
+            if(c=='b'||c=='a'||c=='l'||c=='o'||c=='n') mpp[c]++;
+        }
+        if(mpp.size()!=5) return 0;
+        int num=INT_MAX;
+        for(auto it:mpp)
+        {
+            num=min(num,it.second);
+            if(mpp['l']<2) return 0;
+            else if(mpp['o']<2) return 0;
+            cout<<it.first<<" "<<it.second<<endl;
+        }
+        if(mpp['l']>=2*num&&mpp['o']>=2*num) return num;
+        return min(mpp['l']/2,mpp['o']/2);
+    }
+};
