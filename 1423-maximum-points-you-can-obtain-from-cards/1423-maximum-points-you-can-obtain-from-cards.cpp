b077e9821c62ecdd11653a704c87defad0d6f4be
V PAVAN SATYA PRASAD <vpavan1242005@gmail.com>
2026-06-01 16:40:01 +0530

    Time: 0 ms (100%), Space: 46.2 MB (62.85%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -1,33 +1,20 @@
 class Solution {
 public:
     int maxScore(vector<int>& nums, int k) {
-        vector<int>temp1;
-        vector<int>temp2;
-        temp1.push_back(nums[0]);
-        for(int i=1;i<k;i++)
+        int lsum=0,ans=0;
+        for(int i=0;i<k;i++)
         {
-            temp1.push_back(temp1[i-1]+nums[i]);
+            lsum+=nums[i];
         }
-        if(nums.size()<=k) return temp1[temp1.size()-1];
-        temp2.push_back(nums[nums.size()-1]);
-        int in=0;
-        for(int i=nums.size()-2;i>=nums.size()-k;i--) 
+        ans=lsum;
+        int ind=nums.size()-1;
+        int rsum=0;
+        for(int i=k-1;i>=0;i--)
         {
-            temp2.push_back(temp2[in]+nums[i]);
-            in++;
-        }
-        int ans=max(temp1[k-1],temp2[temp2.size()-1]);
-        int ind=temp2.size()-2;
-        for(int i=0;i<temp1.size()-1;i++)
-        {
-            ans=max(temp1[i]+temp2[ind],ans);
+            lsum-=nums[i];
+            rsum+=nums[ind];
             ind--;
-        }
-        ind=temp1.size()-2;
-        for(int i=0;i<temp2.size()-1;i++)
-        {
-            ans=max(temp2[i]+temp1[ind],ans);
-            ind--;
+            ans=max(ans,lsum+rsum);
         }
         return ans;
     }
