aac2f8a850cd53aff9340916bf179519aba2c0e7
V PAVAN SATYA PRASAD <vpavan1242005@gmail.com>
2026-06-05 00:02:37 +0530

    Time: 7 ms (45.41%), Space: 11.3 MB (5.67%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,35 +1,27 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int l=0,r=0,ans=0,sum=0;
+        int l=0,r=0,ans=0;
         unordered_map<char,int>mpp;
+        int maxfreq=0;
         while(r<s.length())
         {
             mpp[s[r]]++;
-            sum++;
+            maxfreq=max(maxfreq,mpp[s[r]]);
             if(mpp.size()>k+1)
             {
                 mpp[s[l]]--;
-                sum--;
                 if(mpp[s[l]]==0) mpp.erase(s[l]);
                 l++;
             }
             if(mpp.size()<=k+1)
             {
-                int mark=0;
-                for(auto it:mpp)
+                if((r-l+1)-maxfreq<=k)
                 {
-                    if(sum-it.second<=k)
-                    {
-                        ans=max(ans,r-l+1);
-                        mark=1;
-                        break;
-                    }
+                    ans=max(ans,r-l+1);
                 }
-                if(!mark)
-                {
+                else{
                     mpp[s[l]]--;
-                    sum--;
                     if(mpp[s[l]]==0) mpp.erase(s[l]);
                     l++;
                 }
