e7c6fec381ebd661a6a61c00819733a4800a9d74
V PAVAN SATYA PRASAD <vpavan1242005@gmail.com>
2026-06-09 15:43:19 +0530

    Time: 4 ms (33.07%), Space: 103.6 MB (96.89%) - LeetHub

--- 3689-maximum-total-subarray-value-i/3689-maximum-total-subarray-value-i.cpp
+++ 3689-maximum-total-subarray-value-i/3689-maximum-total-subarray-value-i.cpp
@@ -8,16 +8,6 @@ public:
             maxi=max(maxi,i);
             mini=min(mini,i);
         }
-        if(maxi!=mini)
-        {
-            return (long long)(maxi-mini)*k;
-        }
-        int mini1=INT_MAX;
-        for(int i:nums)
-        {
-            if(i!=mini) mini1=min(mini1,i);
-        }
-        if(mini1==INT_MAX) return 0;
-        return (long long)(maxi-mini1)*k;
+        return 1LL*(maxi-mini)*k;
     }
 };
