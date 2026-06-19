aab3cb469e124062796e7d7c9a5f301e6e5fadb1
V PAVAN SATYA PRASAD <vpavan1242005@gmail.com>
2026-06-20 01:11:26 +0530

    Time: 3 ms (40.53%), Space: 82.1 MB (90.83%) - LeetHub

---
+++ 2105-watering-plants-ii/2105-watering-plants-ii.cpp
@@ -0,0 +1,82 @@
+class Solution {
+public:
+    int minimumRefill(vector<int>& plants, int capacityA, int capacityB) {
+        int l=0,r=plants.size()-1,ans=0;
+        int cap1=capacityA;
+        int cap2=capacityB;
+        while(l<=r)
+        {
+            if(l==r)
+            {
+                if(cap1>cap2)
+                {
+                    if(cap1>=plants[l])
+                    {
+                        cap1-=plants[l];
+                        l++;
+                    }
+                    else
+                    {
+                        ans++;
+                        cap1=capacityA-plants[l];
+                        l++;
+                    }
+                }
+                else if(cap1<cap2)
+                {
+                    if(cap2>=plants[r])
+                    {
+                        cap2-=plants[r];
+                        r--;
+                    }
+                    else
+                    {
+                        ans++;
+                        cap2=capacityB-plants[r];
+                        r--;
+                    }
+                }
+                else if(cap1==cap2)
+                {
+                    if(cap1>=plants[l])
+                    {
+                        cap1-=plants[l];
+                        l++;
+                    }
+                    else
+                    {
+                        ans++;
+                        cap1=capacityA-plants[l];
+                        l++;
+                    }
+                }
+            }
+            else
+            {
+                if(plants[l]<=cap1)
+                {
+                    cap1-=plants[l];
+                    l++;
+                }
+                else if(plants[l]>cap1)
+                {
+                    ans++;
+                    cap1=capacityA-plants[l];
+                    l++;
+                }
+                if(plants[r]<=cap2)
+                {
+                    cap2-=plants[r];
+                    r--;
+                }
+                else if(plants[r]>cap2)
+                {
+                    ans++;
+                    cap2=capacityB-plants[r];
+                    r--;
+                }
+            }
+        }
+        return ans;
+    }
+};
