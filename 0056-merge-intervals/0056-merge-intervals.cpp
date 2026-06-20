a9877119035a5fe59024033ffcbe24d3f43d65ae
V PAVAN SATYA PRASAD <vpavan1242005@gmail.com>
2026-06-20 23:11:42 +0530

    Time: 4 ms (65.79%), Space: 23.8 MB (59.06%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         sort(intervals.begin(),intervals.end());
-        for(int i=0;i<intervals.size();i++) cout<<intervals[i][0]<<" "<<intervals[i][1]<<endl;
         int num=intervals[0][1];
         int ind=0;
         vector<vector<int>>ans;
