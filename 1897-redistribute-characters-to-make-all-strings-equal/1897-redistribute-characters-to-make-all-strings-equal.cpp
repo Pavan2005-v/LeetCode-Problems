529dea7c66fb82d7f3de3ad71b4163fec75807e8
V PAVAN SATYA PRASAD <vpavan1242005@gmail.com>
2026-06-20 18:46:50 +0530

    Time: 7 ms (14.84%), Space: 15.7 MB (8.59%) - LeetHub

--- 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.cpp
+++ 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.cpp
@@ -1,15 +1,10 @@
 class Solution {
 public:
     bool makeEqual(vector<string>& words) {
-        if(words.size()==1) return true;
         unordered_map<char,int>mpp;
-        int mini=INT_MAX;
-        for(int i=0;i<words.size();i++)
+        for(string word:words)
         {
-            for(int j=0;j<words[i].size();j++)
-            {
-                mpp[words[i][j]]++;
-            }
+            for(char c:word) mpp[c]++;  
         }
         for(auto it:mpp)
         {
