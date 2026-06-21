0bcd554728b94f5865a63b2281f5d02535e382fa
V PAVAN SATYA PRASAD <vpavan1242005@gmail.com>
2026-06-21 22:52:27 +0530

    Time: 18 ms (39.68%), Space: 22.4 MB (5.09%) - LeetHub

--- 3968-maximum-manhattan-distance-after-all-moves/3968-maximum-manhattan-distance-after-all-moves.cpp
+++ 3968-maximum-manhattan-distance-after-all-moves/3968-maximum-manhattan-distance-after-all-moves.cpp
@@ -1,35 +1,39 @@
 class Solution {
 public:
     int maxDistance(string moves) {
-        unordered_map<int,int>mpp;
         int maxi=INT_MIN;
         int a=0,b=0,c1=0;
+        int r=0,l=0,u=0,d=0;
         for(char c:moves)
         {
-            if(c!='_')
+            if(c=='R')
             {
-                mpp[c]++;
+                a++; 
+                r++;
             }
-            if(c=='R') a++;
-            else if(c=='L') a--;
-            else if(c=='U') b++;
-            else if(c=='D') b--;
+            else if(c=='L')
+            {
+                a--; 
+                l++;
+            }
+            else if(c=='U')
+            {
+                b++; 
+                u++;
+            }
+            else if(c=='D')
+            {
+                b--; 
+                d++;
+            }
             else if(c=='_') c1++;
         }
-        //LU_D_U
         char c;
-        for(auto it:mpp)
-        {
-            if(it.second>maxi)
-            {
-                maxi=it.second;
-                c=it.first;
-            }
-        }
-        if(c=='U') b+=c1;
-        else if(c=='D') b-=c1;
-        else if(c=='L') a-=c1;
-        else if(c=='R') a+=c1;
+        maxi=max({l,r,u,d});
+        if(maxi==u) b+=c1;
+        else if(maxi==d) b-=c1;
+        else if(maxi==l) a-=c1;
+        else if(maxi==r) a+=c1;
         return abs(a)+abs(b);
     }
 };
