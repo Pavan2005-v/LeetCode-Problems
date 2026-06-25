6d2b1d4644990e3e12cd2b0d6532760a917ff4aa
V PAVAN SATYA PRASAD <vpavan1242005@gmail.com>
2026-06-25 18:04:46 +0530

    Time: 0 ms (100%), Space: 8.2 MB (7.43%) - LeetHub

--- 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.cpp
+++ 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.cpp
@@ -7,17 +7,16 @@ public:
         for(int i=0;i<k;i++)
         {
             win+=(s[i]);
-            n=n*10+(s[i]-'0');
         }
         int l=0,r=k-1,ans=0;
+        n=stoi(win);
         if(n!=0&&num%n==0) ans++;
         while(r<s.length()-1)
         {
             l++;
             r++;
             win=s.substr(l,r-l+1);
-            n=0;
-            for(int i=0;i<win.length();i++) n=n*10+(win[i]-'0');
+            n=stoi(win);
             if(n!=0&&num%n==0) ans++;
         }
         return ans;
