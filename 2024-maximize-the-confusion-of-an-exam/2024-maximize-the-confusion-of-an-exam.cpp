fa74de09510594a3a38f2ec6fe002b45c6b63f33
V PAVAN SATYA PRASAD <vpavan1242005@gmail.com>
2026-06-26 23:57:24 +0530

    Time: 1 ms (94.91%), Space: 12.5 MB (77.43%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    int MaxF(string s,int k,char c)
-    {
-        int l=0,r=0,count=0,ans=0;
-        while(r<s.length())
+    int maxConsecutiveAnswers(string answerKey, int k) {
+        int l=0,r=0,count1=0,count2=0,ans=0;
+        while(r<answerKey.size())
         {
-            if(s[r]==c) count++;
+            if(answerKey[r]=='T') count1++;
+            else count2++;
+            int count=min(count1,count2);
             if(count>k)
             {
-                if(s[l]==c) count--;
+                if(answerKey[l]=='T') count1--;
+                else count2--;
                 l++;
             }
             if(count<=k) ans=max(ans,r-l+1);
             r++;
         }
-        return ans;   
-    }
-    int maxConsecutiveAnswers(string answerKey, int k) {
-        return max(MaxF(answerKey,k,'F'),MaxF(answerKey,k,'T'));
+        return ans;
     }
 };
