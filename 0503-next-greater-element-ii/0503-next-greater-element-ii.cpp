1357949065ba9778bb159c6c14d99d2100a488e7
V PAVAN SATYA PRASAD <vpavan1242005@gmail.com>
2026-06-27 16:12:45 +0530

    Time: 173 ms (5.05%), Space: 28.4 MB (62.82%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -5,27 +5,26 @@ public:
         vector<int>ans;
         while(l<nums.size())
         {
+            if(r>=nums.size())
+            {
+                r=0;
+            }
             if(r==l)
             {
                 ans.push_back(-1);
                 l++;
-                r=l+1;
-            }
-            if(r>=nums.size())
-            {
-                r=0;
-            }
-            if(l<nums.size()&&nums[l]<nums[r])
-            {
-                ans.push_back(nums[r]);
-                l++;
                 r=l;
             }
-            r++;
-            if(r>=nums.size())
+            if(l<nums.size())
             {
-                r=0;
+                if(nums[l]<nums[r])
+                {
+                    ans.push_back(nums[r]);
+                    l++;
+                    r=l;
+                }
             }
+            r++;
         }
         return ans;
     }
