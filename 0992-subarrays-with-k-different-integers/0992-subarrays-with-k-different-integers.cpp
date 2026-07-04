65435e40d0bf61a890f94c412b126cbb4931bc16
V PAVAN SATYA PRASAD <vpavan1242005@gmail.com>
2026-07-04 13:17:13 +0530

    Time: 87 ms (44.93%), Space: 58.4 MB (17.37%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int func(vector<int>nums,int k)
     {
-        if(k<0) return -1;
-        unordered_map<int,int>mpp;
+        if(k==0) return 0;
         int l=0,r=0,ans=0;
+        unordered_map<int,int>mpp;
         while(r<nums.size())
         {
             mpp[nums[r]]++;
@@ -14,7 +14,7 @@ public:
                 if(mpp[nums[l]]==0) mpp.erase(nums[l]);
                 l++;
             }
-            ans+=(r-l+1);
+            if(mpp.size()<=k) ans+=(r-l+1);
             r++;
         }
         return ans;
