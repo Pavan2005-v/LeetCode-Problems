554af44313e9d263a7eb3f236ce949c7593eee10
V PAVAN SATYA PRASAD <vpavan1242005@gmail.com>
2026-07-04 17:31:34 +0530

    Time: 117 ms (91.35%), Space: 118.9 MB (78.78%) - LeetHub

--- 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.cpp
+++ 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.cpp
@@ -3,16 +3,13 @@ public:
     int minimumCardPickup(vector<int>& nums) {
         int l=0,r=0,ans=INT_MAX;
         unordered_map<int,int>mpp;
-        while(r<nums.size())
+        for(int i=0;i<nums.size();i++)
         {
-            mpp[nums[r]]++;
-            while(mpp[nums[r]]>1)
+            if(mpp.contains(nums[i]))
             {
-                ans=min(ans,r-l+1);
-                mpp[nums[l]]--;
-                l++;
+                ans=min(ans,i-mpp[nums[i]]+1);
             }
-            r++;
+            mpp[nums[i]]=i;
         }
         return (ans!=INT_MAX)?ans:-1;
     }
