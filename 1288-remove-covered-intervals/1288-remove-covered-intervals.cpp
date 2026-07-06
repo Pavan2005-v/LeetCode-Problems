805852ba8f908e594689e4b3a58d31630dbd8b82
V PAVAN SATYA PRASAD <vpavan1242005@gmail.com>
2026-07-06 14:26:00 +0530

    Time: 1 ms (52.98%), Space: 15.1 MB (60.13%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -1,31 +1,23 @@
 class Solution {
 public:
-    int removeCoveredIntervals(vector<vector<int>>& nums) {
-        sort(nums.begin(),nums.end(),[](const vector<int>&a,const vector<int>&b){
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end(),[&](vector<int>&a,vector<int>&b){
             if(a[0]==b[0])
             {
-                return a[1]<b[1];
+                return a[1]>b[1];
             }
-            return a[0]>b[0];
+            return a[0]<b[0];
         });
-        vector<int>temp(nums.size());
-        int ans=0;
-        for(int i=0;i<nums.size();i++)
+        int count=1;
+        int maxi=intervals[0][1];
+        for(int i=1;i<intervals.size();i++)
         {
-            int pos1=nums[i][0];
-                int pos2=nums[i][1];
-                for(int j=i-1;j>=0;j--)
-                {
-                    if(temp[j]==0)
-                    {
-                        if(nums[j][0]>=pos1&&nums[j][1]<=pos2)
-                        {
-                            ans++;
-                            temp[j]=1;
-                        }
-                    }
-                }
+            if(intervals[i][0]>maxi||intervals[i][1]>maxi)
+            {
+                count++;
+            }
+            maxi=max(maxi,intervals[i][1]);
         }
-        return nums.size()-ans;
+        return count;
     }
 };
