1a84cb4f00d5443edcbc86d07dc5a43b4ec349db
V PAVAN SATYA PRASAD <vpavan1242005@gmail.com>
2026-07-10 12:09:31 +0530

    Time: 0 ms (100%), Space: 34.5 MB (40.55%) - LeetHub

--- 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.cpp
+++ 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.cpp
@@ -1,55 +1,28 @@
 class Solution {
 public:
     vector<int> resultsArray(vector<int>& nums, int k) {
+        int l=0,r=k-1,pt=0,count=0;
         vector<int>ans;
-        int l=0,r=k-1;
-        long long sum=0;
-        for(int i=0;i<k;i++)
+        for(int i=0;i<k-1;i++)
         {
-            sum+=nums[i];
+            if(nums[i+1]-nums[i]==1) count++;
+            else count=0;
         }
-        int num=nums[0]-1,num1=nums[k-1];
-        long long exSum=((long long)num1*(num1+1)/2)-((long long)num*(num+1)/2);
-        if(sum==exSum)
+        if(count!=k-1)
         {
-            int m=0;
-            for(int i=0;i<k-1;i++)
-            {
-                if(nums[i]+1!=nums[i+1])
-                {
-                    m=1;
-                    ans.push_back(-1);
-                    break;
-                }
-            }
-            if(!m) ans.push_back(nums[r]);
+            ans.push_back(-1);
+            // count=0;
         }
-        else ans.push_back(-1);
+        else ans.push_back(nums[k-1]);
         while(r<nums.size()-1)
         {
-            sum-=nums[l];
             l++;
             r++;
-            sum+=nums[r];
-            int num=nums[l]-1,num1=nums[r];
-            long long exSum=((long long)num1*(num1+1)/2)-((long long)num*(num+1)/2);
-            if(sum==exSum)
-            {
-                int m=0;
-                for(int i=l;i<r;i++)
-                {
-                    if(nums[i]+1!=nums[i+1])
-                    {
-                        m=1;
-                        ans.push_back(-1);
-                        break;
-                    }
-                }
-                if(!m) ans.push_back(nums[r]);
-            }
+            if(nums[r]-nums[r-1]==1) count++;
+            else count=0;
+            if(count>=k-1) ans.push_back(nums[r]);
             else ans.push_back(-1);
         }
-        
         return ans;
     }
 };
