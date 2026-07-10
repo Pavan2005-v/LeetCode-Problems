563febcd9f50e0f549b452c61b00bdd0468dda5c
V PAVAN SATYA PRASAD <vpavan1242005@gmail.com>
2026-07-10 12:55:23 +0530

    Time: 275 ms (6.73%), Space: 409.5 MB (5.38%) - LeetHub

--- 1754-largest-merge-of-two-strings/1754-largest-merge-of-two-strings.cpp
+++ 1754-largest-merge-of-two-strings/1754-largest-merge-of-two-strings.cpp
@@ -1,34 +1,21 @@
 class Solution {
 public:
     string largestMerge(string word1, string word2) {
-        string ans="";
         int l=0,r=0;
+        string ans="";
         while(l<word1.length()||r<word2.length())
         {
-            if(l<word1.length()&&r<word2.length()&&word1[l]>word2[r])
+            string temp1=word1.substr(l);
+            string temp2=word2.substr(r);
+            if(temp1>temp2)
             {
                 ans+=word1[l];
                 l++;
             }
-            else if(l<word1.length()&&r<word2.length()&&word1[l]<word2[r])
+            else
             {
                 ans+=word2[r];
                 r++;
-            }
-            else
-            {
-                string temp1=word1.substr(l);
-                string temp2=word2.substr(r);
-                if(temp1>temp2)
-                {
-                    ans+=word1[l];
-                    l++;
-                }
-                else
-                {
-                    ans+=word2[r];
-                    r++;
-                }
             }
         }
         return ans;
