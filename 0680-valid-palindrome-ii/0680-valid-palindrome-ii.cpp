0f4cf5ae5d7075d66b7256be971689b8941e95f9
V PAVAN SATYA PRASAD <vpavan1242005@gmail.com>
2026-07-21 20:24:10 +0530

    Time: 3 ms (67.91%), Space: 22.2 MB (57.46%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -1,43 +1,43 @@
 class Solution {
 public:
     bool validPalindrome(string s) {
-        int l=0,r=s.length()-1,limit=0,k=0;
-        while(l<=r)
+        int l=0,r=s.length()-1,count1=-1,count2=-1;
+        while(l<r)
         {
             if(s[l]!=s[r])
             {
-                limit++;
-                k=1;
+                count1=l;
+                count2=r;
                 break;
             }
-            l++;
-            r--;
+            else l++; r--;
         }
-        if(limit>1) return false;
-        if(!k) return true;
-        string temp=s;
-        int ind=r;
-        s.erase(l,1);
-        l=0,r=s.length()-1,k=0;
-        while(l<=r)
+        if(l>=r) return true;
+        l++;
+        while(l<r)
         {
             if(s[l]!=s[r])
             {
-                k=1;
                 break;
             }
-            l++;
-            r--; 
+            else
+            {
+                l++; r--;
+            }
         }
-        if(!k) return true;
-        r=ind;
-        temp.erase(r,1);
-        l=0,r=temp.length()-1;
-        while(l<=r)
+        if(l>=r) return true;
+        l=count1;
+        r=count2-1;
+        while(l<r)
         {
-            if(temp[l]!=temp[r]) return false;
-            l++;
-            r--; 
+            if(s[l]!=s[r])
+            {
+                return false;
+            }
+            else
+            {
+                l++; r--;
+            }
         }
         return true;
     }
