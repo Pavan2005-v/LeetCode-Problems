199bd37bc51e8d70b95d72c80da471d8daf9d10a
V PAVAN SATYA PRASAD <vpavan1242005@gmail.com>
2026-07-26 22:02:52 +0530

    Time: 0 ms (100%), Space: 8.6 MB (43%) - LeetHub

--- 2287-rearrange-characters-to-make-target-string/2287-rearrange-characters-to-make-target-string.cpp
+++ 2287-rearrange-characters-to-make-target-string/2287-rearrange-characters-to-make-target-string.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
     int rearrangeCharacters(string s, string target) {
+        unordered_map<char,int>mpp,mpp1;
+        for(char c:s) mpp[c]++;
+        for(char c:target) mpp1[c]++;
         int ans=INT_MAX;
-        unordered_map<char,int>mpp1;
-        unordered_map<char,int>mpp2;
-        for(char c:target) mpp2[c]++;
-        for(char c:s)
-        {
-            if(mpp2.contains(c)) mpp1[c]++;
-        }
-        for(auto it:mpp2)
-        {
-            if(it.second>mpp1[it.first]) return 0;
-        }
         for(auto it:mpp1)
         {
-            ans=min(ans,it.second/mpp2[it.first]);
+            ans=min(ans,mpp[it.first]/mpp1[it.first]);
         }
-        return ans;
+        return ans==INT_MAX?0:ans;
     }
 };
