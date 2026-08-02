42fb9c5defee4185a71670aeacfbc7ba8de5e856
V PAVAN SATYA PRASAD <vpavan1242005@gmail.com>
2026-08-02 14:40:28 +0530

    Time: 7 ms (10.57%), Space: 32.4 MB (6.23%) - LeetHub

--- 3364-minimum-positive-sum-subarray/3364-minimum-positive-sum-subarray.cpp
+++ 3364-minimum-positive-sum-subarray/3364-minimum-positive-sum-subarray.cpp
@@ -1,22 +1,34 @@
 class Solution {
 public:
-    int minimumSumSubarray(vector<int>& nums, int minSize, int maxSize) {
-        int ans=INT_MAX;
-        for(int len=minSize;len<=maxSize;len++)
+    int func(vector<int>nums,int k)
+    {
+        int ans=INT_MAX,sum=0;
+        for(int i=0;i<k;i++)
         {
-            int sum=0;
-            for(int i=0;i<len;i++) sum+=nums[i];
-            int l=0,r=len-1;
+            sum+=nums[i];
+        }
+        if(sum>0) ans=min(ans,sum);
+        int l=0,r=k-1;
+        while(r<nums.size()-1)
+        {
+            sum-=nums[l];
+            l++;
+            r++;
+            sum+=nums[r];
             if(sum>0) ans=min(ans,sum);
-            while(r<nums.size()-1)
+        }
+        return ans==INT_MAX?0:ans;
+    }
+    int minimumSumSubarray(vector<int>& nums, int l, int r) {
+        int ans=INT_MAX;
+        for(int i=l;i<=r;i++)
+        {
+            int temp=func(nums,i);
+            if(temp!=0)
             {
-                sum-=nums[l];
-                l++;
-                r++;
-                sum+=nums[r];
-                if(sum>0) ans=min(ans,sum);
+                ans=min(ans,temp);
             }
         }
-        return (ans!=INT_MAX)?ans:-1;
+        return ans==INT_MAX?-1:ans;
     }
 };
