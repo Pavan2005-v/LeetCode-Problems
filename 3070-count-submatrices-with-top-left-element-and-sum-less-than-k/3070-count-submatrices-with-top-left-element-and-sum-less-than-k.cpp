79d531367c46d903befb3b553804e7f7b56c9b91
V PAVAN SATYA PRASAD <vpavan1242005@gmail.com>
2026-08-03 15:11:39 +0530

    Time: 2 ms (97.07%), Space: 90.6 MB (67.07%) - LeetHub

--- 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.cpp
+++ 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.cpp
@@ -2,13 +2,23 @@ class Solution {
 public:
     int countSubmatrices(vector<vector<int>>& grid, int k) {
         int ans=0,n=grid.size(),m=grid[0].size();
-        vector<vector<int>>pref(n+1,vector<int>(m+1));
-        for(int i=1;i<n+1;i++)
+        if(grid[0][0]<=k) ans++;
+        for(int i=1;i<n;i++)
         {
-            for(int j=1;j<m+1;j++)
+            grid[i][0]+=grid[i-1][0];
+            if(grid[i][0]<=k) ans++;
+        }
+        for(int i=1;i<m;i++)
+        {
+            grid[0][i]+=grid[0][i-1];
+            if(grid[0][i]<=k) ans++;
+        }
+        for(int i=1;i<n;i++)
+        {
+            for(int j=1;j<m;j++)
             {
-                pref[i][j]=grid[i-1][j-1]+pref[i-1][j]+pref[i][j-1]-pref[i-1][j-1];
-                if(pref[i][j]<=k) ans++;
+                grid[i][j]=grid[i][j]+grid[i-1][j]+grid[i][j-1]-grid[i-1][j-1];
+                if(grid[i][j]<=k) ans++;
             }
         }
         return ans;
