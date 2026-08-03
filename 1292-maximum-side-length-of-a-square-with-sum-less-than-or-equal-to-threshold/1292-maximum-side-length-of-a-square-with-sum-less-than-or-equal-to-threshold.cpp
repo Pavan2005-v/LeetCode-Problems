a9f03430ee970f69b032506720d810d4b155f490
V PAVAN SATYA PRASAD <vpavan1242005@gmail.com>
2026-08-03 11:40:08 +0530

    Time: 189 ms (17.01%), Space: 41.1 MB (31.8%) - LeetHub

---
+++ 1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int maxSideLength(vector<vector<int>>& mat, int threshold) {
+        int n=mat.size(),m=mat[0].size();
+        vector<vector<int>>pref(n+1,vector<int>(m+1));
+        for(int i=1;i<n+1;i++)
+        {
+            for(int j=1;j<m+1;j++)
+            {
+                pref[i][j]=mat[i-1][j-1]+pref[i-1][j]+pref[i][j-1]-pref[i-1][j-1];
+            }
+        }
+        // int l=1,r=min(n,m),ans=0;
+        for(int size=min(n,m);size>=0;size--)
+        {
+            // int size=(l+r)/2;
+            // bool a=false;
+            for(int i=0;i<n-size+1;i++)
+            {
+                for(int j=0;j<m-size+1;j++)
+                {
+                    int ind1=size+i-1,ind2=size+j-1;
+                    int sum=pref[ind1+1][ind2+1]-pref[ind1+1][j]-pref[i][ind2+1]+pref[i][j];
+                    if(sum<=threshold)
+                    {
+                        return size;
+                    }
+                }
+                // if(a) break;
+            }
+            // if(a)
+            // {
+            //     ans=size;
+            //     l=size+1;
+            // }
+            // else
+            // {
+            //     r=size-1;
+            // }
+        }
+        return 0;
+    }
+};
