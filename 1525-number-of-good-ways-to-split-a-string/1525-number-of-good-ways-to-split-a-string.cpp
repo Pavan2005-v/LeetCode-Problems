529f6abd201af80f3b27c4230e65ee55c97f695d
V PAVAN SATYA PRASAD <vpavan1242005@gmail.com>
2026-08-05 14:50:18 +0530

    Time: 0 ms (100%), Space: 11.4 MB (87.97%) - LeetHub

--- 1525-number-of-good-ways-to-split-a-string/1525-number-of-good-ways-to-split-a-string.cpp
+++ 1525-number-of-good-ways-to-split-a-string/1525-number-of-good-ways-to-split-a-string.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     int numSplits(string s) {
-        int n=s.length(),ans=0;
-        unordered_map<char,int>left,right;
+        int n=s.length(),ans=0,count1=0,count2=0;
+        vector<int>left(26),right(26);
         for(int i=1;i<n;i++)
         {
-            right[s[i]]++;
+            if(right[s[i]-'a']==0) count2++;
+            right[s[i]-'a']++;
         }
         for(int i=1;i<n;i++)
         {
-            left[s[i-1]]++;
-            if(left.size()==right.size()) ans++;
-            right[s[i]]--;
-            if(right[s[i]]==0) right.erase(s[i]);
+            if(left[s[i-1]-'a']==0) count1++;
+            left[s[i-1]-'a']++;
+            if(count1==count2) ans++;
+            right[s[i]-'a']--;
+            if(right[s[i]-'a']==0) count2--;
         }
         return ans;
     }
