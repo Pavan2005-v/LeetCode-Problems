c0ff936864e1ed9dbb513f7840a3845596b6810c
V PAVAN SATYA PRASAD <vpavan1242005@gmail.com>
2026-08-24 15:26:24 +0530

    Time: 0 ms (100%), Space: 30.4 MB (42.76%) - LeetHub

--- 1720-decode-xored-array/1720-decode-xored-array.cpp
+++ 1720-decode-xored-array/1720-decode-xored-array.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     vector<int> decode(vector<int>& encoded, int first) {
-        vector<int>ans;
-        ans.push_back(first);
-        for(int i=0;i<encoded.size();i++)
+        int n=encoded.size();
+        vector<int>ans={first};
+        for(int i=0;i<n;i++)
         {
-            int k=encoded[i]^ans[ans.size()-1];
-            ans.push_back(k);
+            ans.push_back(encoded[i]^ans[i]);
         }
         return ans;
     }
