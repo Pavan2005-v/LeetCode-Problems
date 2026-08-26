4eb1e69890be0aa24bbe98f4baf433225fe03c0c
V PAVAN SATYA PRASAD <vpavan1242005@gmail.com>
2026-08-26 15:01:56 +0530

    Time: 0 ms (100%), Space: 8.7 MB (64.74%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
     string shortestBeautifulSubstring(string s, int k) {
-        int l=0,r=0,count=0,len=INT_MAX;
-        vector<string>temp;
-        while(r<s.length())
+        int n=s.length(),l=0,r=0,len=INT_MAX,count=0;
+        string ans="zyxwvutsrqponmlkjihgfedcba";
+        while(r<n)
         {
-            if(s[r]=='1') count++;
+            if(s[r]=='1')
+            {
+                count++;
+            }
             while(count==k)
             {
-                len=min(len,r-l+1);
-                temp.push_back(s.substr(l,r-l+1));
+                if(r-l+1<len)
+                {
+                    len=r-l+1;
+                    ans=s.substr(l,r-l+1);
+                }
+                if(r-l+1==len)
+                {
+                    ans=min(ans,s.substr(l,r-l+1));
+                }
                 if(s[l]=='1') count--;
                 l++;
             }
             r++;
-        }
-        string ans="zyxwvutsrqponmlkjihgfedcba";
-        for(int i=0;i<temp.size();i++)
-        {
-            if(temp[i].length()==len)
-            {
-                if(ans>temp[i])
-                {
-                    ans=temp[i];
-                }
-            }
         }
         return len==INT_MAX?"":ans;
     }
