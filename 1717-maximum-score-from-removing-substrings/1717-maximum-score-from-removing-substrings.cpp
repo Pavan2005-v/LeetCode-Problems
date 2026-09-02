ec3179a6856887877684c66e5958ec60ca710122
V PAVAN SATYA PRASAD <vpavan1242005@gmail.com>
2026-09-02 11:35:18 +0530

    Time: 41 ms (52.58%), Space: 31.5 MB (30.12%) - LeetHub

--- 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.cpp
+++ 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.cpp
@@ -4,6 +4,7 @@ public:
     {
         int n=s.length(),score=0;
         stack<char>st;
+        string temp="";
         for(int i=0;i<n;i++)
         {
             if(st.empty())
@@ -23,16 +24,31 @@ public:
                 }
             }
         }
-        string temp="";
         while(!st.empty())
         {
-            if(temp.length()!=0&&temp[temp.length()-1]=='a'&&st.top()=='b')
-            {
-                temp.erase(temp.length()-1,1);
-                score+=y;
-            }
-            else temp+=st.top();
+            temp+=st.top();
             st.pop();
+        }
+        reverse(temp.begin(),temp.end());
+        stack<char>st1;
+        for(int i=0;i<temp.length();i++)
+        {
+            if(st1.empty())
+            {
+                st1.push(temp[i]);
+            }
+            else
+            {
+                if(st1.top()=='b'&&temp[i]=='a')
+                {
+                    score+=y;
+                    st1.pop();
+                }
+                else
+                {
+                    st1.push(temp[i]);
+                }
+            }
         }
         return score;
     }
@@ -40,6 +56,7 @@ public:
     {
         int n=s.length(),score=0;
         stack<char>st;
+        string temp="";
         for(int i=0;i<n;i++)
         {
             if(st.empty())
@@ -58,17 +75,34 @@ public:
                     st.push(s[i]);
                 }
             }
+            // cout<<st.top()<<endl;
         }
-        string temp="";
+        // cout<<score;
         while(!st.empty())
         {
-            if(temp.length()!=0&&temp[temp.length()-1]=='b'&&st.top()=='a')
-            {
-                temp.erase(temp.length()-1,1);
-                score+=x;
-            }
-            else temp+=st.top();
+            temp+=st.top();
             st.pop();
+        }
+        reverse(temp.begin(),temp.end());
+        stack<char>st1;
+        for(int i=0;i<temp.length();i++)
+        {
+            if(st1.empty())
+            {
+                st1.push(temp[i]);
+            }
+            else
+            {
+                if(st1.top()=='a'&&temp[i]=='b')
+                {
+                    score+=x;
+                    st1.pop();
+                }
+                else
+                {
+                    st1.push(temp[i]);
+                }
+            }
         }
         // cout<<score;
         return score;
